f7f4af11e518838553fd597a81ee75fc9a9db010
SongSeungJu <wkwjsrj5079@gmail.com>
2024-07-21 13:46:01 +0900

    jwt update but yet do not test

--- src/main/java/org/crazyspace_edu/api/config/SecurityConfig.java
+++ src/main/java/org/crazyspace_edu/api/config/SecurityConfig.java
@@ -1,17 +1,22 @@
 package org.crazyspace_edu.api.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.crazyspace_edu.api.config.filter.EmailPasswordAuthFilter;
+import org.crazyspace_edu.api.config.filter.JwtFilter;
 import org.crazyspace_edu.api.config.handler.Http401Handler;
 import org.crazyspace_edu.api.config.handler.Http403Handler;
 import org.crazyspace_edu.api.config.handler.LoginFailHandler;
 import org.crazyspace_edu.api.config.handler.LoginSuccessHandler;
 import org.crazyspace_edu.api.domain.user.User;
 import org.crazyspace_edu.api.repository.UserRepository;
+import org.crazyspace_edu.api.service.AuthService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -19,6 +24,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -31,43 +37,48 @@ import org.springframework.session.security.web.authentication.SpringSessionReme
 
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
-@Slf4j
 @Configuration
-@EnableWebSecurity // 하나씩 사용자 권한이 필요한 url를 넣기 귀찬음 어노테이션으로 대체
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     final UserRepository userRepository;
     private final ObjectMapper objectMapper;
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    // AuthService 주입 제거
+    // @Lazy
+    // private final AuthService authService;
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
+    public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers("/favicon.ico")
                 .requestMatchers("error");
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 )
+                .addFilterBefore(new JwtFilter(userDetailsService(userRepository), secretKey), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(usernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(
+                        sessionManagement ->
+                                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .exceptionHandling(exception -> {
                     exception.accessDeniedHandler(new Http403Handler(objectMapper));
                     exception.authenticationEntryPoint(new Http401Handler(objectMapper));
                 })
-                .rememberMe(rememberMe -> rememberMe
-                        .rememberMeParameter("remember")
-                        .alwaysRemember(false)
-                        .tokenValiditySeconds(2592000)
-                )
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 
     @Bean
-    public EmailPasswordAuthFilter usernamePasswordAuthenticationFilter(){
-        EmailPasswordAuthFilter filter = new EmailPasswordAuthFilter("/auth/login",objectMapper);
+    public EmailPasswordAuthFilter usernamePasswordAuthenticationFilter() {
+        EmailPasswordAuthFilter filter = new EmailPasswordAuthFilter("/auth/login", objectMapper);
         filter.setAuthenticationManager(authenticationManager());
         filter.setAuthenticationSuccessHandler(new LoginSuccessHandler(objectMapper));
         filter.setAuthenticationFailureHandler(new LoginFailHandler(objectMapper));
@@ -89,8 +100,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository){
-        return new UserDetailsService(){
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
                 User user = userRepository.findByEmail(username)
@@ -98,12 +109,10 @@ public class SecurityConfig {
                 return new UserPrincipal(user);
             }
         };
-
     }
 
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new SCryptPasswordEncoder(
                 16,
                 8,
